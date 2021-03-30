7edad49e218cdf93ebc8f7378063451bd8be6a70
Wiston Venera Macias <wisvem@hotmail.com>
2021-03-30 22:42:05 +0000

    Added tests for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,8 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import unittest
+import os
 
 
 class test_User(test_basemodel):
@@ -32,3 +34,39 @@ class test_User(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.password), str)
+
+
+class TestUser(unittest.TestCase):
+    """ a class for user tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ Example Data """
+        cls.user = User()
+        cls.user.first_name = "Madame"
+        cls.user.last_name = "Tabitha"
+        cls.user.email = "gildedlily@gmail.com"
+        cls.user.password = "gildedlily123"
+
+    @classmethod
+    def teardown(cls):
+        """ Tear down the class """
+        del cls.user
+
+    def tearDown(self):
+        """ Tear down the file (file storage) """
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_docs_user(self):
+        """ check for docstrings """
+        self.assertIsNotNone(User.__doc__)
+
+    def test_attribute_types_User(self):
+        """test attribute type for User"""
+        self.assertEqual(type(self.user.email), str)
+        self.assertEqual(type(self.user.password), str)
+        self.assertEqual(type(self.user.first_name), str)
+        self.assertEqual(type(self.user.first_name), str)
