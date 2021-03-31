cc1a51e114c5fc725869c6643704064c74061adf
Wiston Venera Macias <wisvem@hotmail.com>
2021-03-31 00:24:27 +0000

    Added Diegos tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """Defines unittests for console.py."""
 import os
+import sys
 import pep8
 import unittest
 import models
-from unittest.mock import patch
+from unittest.mock import patch, create_autospec
 from io import StringIO
 from console import HBNBCommand
 from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage as Storage
 from models.engine.file_storage import FileStorage
+
+classes = ["User", "State", "City", "Amenity", "Place", "Review"]
+Storage = Storage()
 
 
 class TestHBNBCommand(unittest.TestCase):
@@ -236,5 +241,38 @@ class TestHBNBCommand(unittest.TestCase):
         my_id = obj[obj.find('(')+1:obj.find(')')]
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestConsole(unittest.TestCase):
+    ''' TestCase class for storing the unittests of the console. '''
+
+    @unittest.skipIf((os.getenv("HBNB_TYPE_STORAGE") == "db"),
+                     "Reason usage of DBStorage")
+    def test_create_00(self):
+        ''' Tests for the create command. '''
+        # Create console session.
+        cons = self.create_session()
+
+        # Tests all classes.
+        for className in classes:
+            # Test "create {} test='TEST'".
+            with patch('sys.stdout', new=StringIO()) as Output:
+                cons.onecmd('create {} test=\"TEST\"'.format(className))
+                create_stdout = Output.getvalue().strip()
+                create_stdout = '{}.{}'.format(className, create_stdout)
+                self.assertTrue(create_stdout in Storage.all())
+
+    @classmethod
+    def tearDown(self):
+        ''' Removes the file.json on each test. '''
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+    def setUp(self):
+        ''' Sets up the mock stdin and stderr. '''
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+
+    def create_session(self, server=None):
+        ''' Creates the cmd session. '''
+        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
