880fcbcd2eee9e332914f02fa10a79f5afe6491f
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-15 12:50:18 +0000

    Task 5 Puppet for setup

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,49 @@
+# 5. Puppet for setup
+
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+# create direcotries /data/web_static/shared/
+file { '/data/web_static/shared/':
+ensure => 'directory'
+}
+->
+# create direcotries /data/web_static/shared/
+file { '/data/web_static/releases/test/':
+ensure => 'directory'
+}
+->
+file { '/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => 'Holberton School',
+  owner   => 'ubuntu',
+  group   => 'ubuntu'
+}
+->
+exec { 'symbolik link':
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  user     => 'root',
+  provider => 'shell'
+}
+->
+file { '/data':
+  ensure  => 'diretory',
+  user    => 'ubuntu',
+  group   => 'ubuntu'
+  recurse => true
+}
+->
+exec { 'Added location':
+  command     => 'sed -i "48i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default',
+  user        => 'root',
+  provider    => 'shell'
+}
+->
+->
+exec { 'Start nginx':
+  command  => 'service nginx restart',
+  user     => 'root',
+  provider => 'shell'
+}
