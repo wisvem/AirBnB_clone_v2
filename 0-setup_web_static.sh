8a1e6ec713baf992aeaab60a1db26299e56c912b
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-15 00:53:08 +0000

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # 0. Prepare your web servers
-mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+apt-get update -y
+apt-get install nginx -y
+mkdir -p /data/web_static/shared/ 
+mkdir -p /data/web_static/releases/test/
 echo "<html>
   <head>
   </head>
@@ -8,14 +11,13 @@ echo "<html>
     Holberton School
   </body>
 </html>" >/data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+rm -rf /data/web_static/current
+ln -snf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-my_string="        location /hbnb_static {
-                alias /home/vagrant/AirBnB_clone_v2/data/web_static/current/
-        }"
-FILE=/etc/nginx/sites-available/default
+my_string="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;/\n\t}\n
+FILE=/etc/nginx/sites-available/default"
 result=$(grep "$my_string" "$FILE")
 if [ -z "$result" ]; then
-    sed -i "48 a $my_string" "$FILE"
+    sed -i "26 a $my_string" "$FILE"
 fi
 service nginx restart
