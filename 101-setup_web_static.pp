a974459eecd0a68473cd0b1a2a52521ba7998f5f
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-15 13:58:29 +0000

    Task 5 - Minor bugs fixes

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -4,58 +4,58 @@ package { 'nginx':
   ensure   => present,
   provider => 'apt'
 }
-->
-file { '/data':
+
+-> file { '/data':
   ensure  => directory,
   owner   => 'ubuntu',
   group   => 'ubuntu',
   recurse => true
 }
-->
+
 # create direcotries /data/web_static/shared/
-file { '/data/web_static/':
+-> file { '/data/web_static':
   ensure => directory,
-  owner   => 'ubuntu'
+  owner  => 'ubuntu'
 }
-->
+
 # create direcotries /data/web_static/shared/
-file { '/data/web_static/shared':
+-> file { '/data/web_static/shared':
   ensure => directory,
-  owner   => 'ubuntu'
+  owner  => 'ubuntu'
 }
-->
+
 # create direcotries /data/web_static/shared/
-file { '/data/web_static/releases/':
+-> file { '/data/web_static/releases':
   ensure => directory,
-  owner   => 'ubuntu'
+  owner  => 'ubuntu'
 }
-->
+
 # create direcotries /data/web_static/shared/
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => directory,
-  owner   => 'ubuntu'
+  owner  => 'ubuntu'
 }
-->
-file { '/data/web_static/releases/test/index.html':
+
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => present,
   content => 'Holberton School',
   owner   => 'ubuntu',
   group   => 'ubuntu'
 }
-->
-exec { 'symbolik link':
+
+-> exec { 'symbolik link':
   command  => 'ln -snf /data/web_static/releases/test/ /data/web_static/current',
   user     => 'ubuntu',
   provider => 'shell'
 }
-->
-exec { 'Added location':
-  command   => 'sed -i "48i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default',
-  user      => 'root',
-  provider  => 'shell'
+
+-> exec { 'Added location':
+  command  => 'sed -i "48i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default',
+  user     => 'root',
+  provider => 'shell'
 }
-->
-exec { 'Start nginx':
+
+-> exec { 'Start nginx':
   command  => 'service nginx restart',
   user     => 'root',
   provider => 'shell'
