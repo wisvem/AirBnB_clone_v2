d82ee96dd33b604a3ee6985a84156b7632e605f8
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-15 03:36:56 +0000

    Task 0 - Added Exit code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
-# 0. Prepare your web servers
+# Prepare Web Server
 apt-get update -y
 apt-get install nginx -y
-mkdir -p /data/web_static/shared/ 
+mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >/data/web_static/releases/test/index.html
-ln -snf /data/web_static/releases/test/ /data/web_static/current
+echo "Holberton School" > /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-my_string="\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-FILE=/etc/nginx/sites-available/default
-result=$(grep "$my_string" "$FILE")
-if [ -z "$result" ]; then
-    sed -i "26 a $my_string" "$FILE"
-fi
+
+loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
+
+sudo sed -i "38i $loc" /etc/nginx/sites-available/default
 service nginx restart
+exit 0
