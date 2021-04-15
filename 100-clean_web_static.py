d9b5973a59b606dfbf70e5b164a26bc0ee19e319
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-15 11:33:18 +0000

    Task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 """ 4. Keep it clean! """
 
 from fabric.api import local, run, env
-from fabric.context_managers import cd
+from fabric.context_managers import cd, lcd
 from fabric.operations import put
 from datetime import datetime
 
@@ -65,5 +65,7 @@ def do_clean(number=0):
         number = int(number)+1
     r_path = "/data/web_static/releases/"
     l_path = "./versions/"
-    local("ls -1t {} | tail -n +{} | xargs rm -rf --".format(l_path, number))
-    run("ls -1t {} | tail -n +{} | xargs rm -rf --".format(r_path, number))
+    with cd(r_path):
+        run("ls -1t | tail -n +{} | xargs rm -rf --".format(number))
+    with lcd(l_path):
+        local("ls -1t | tail -n +{} | xargs rm -rf --".format(number))
