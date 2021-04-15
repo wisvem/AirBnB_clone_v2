776eb55946e7ff727d74d6b769f43cdf5dac7bee
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-15 01:21:17 +0000

    Added .gitignore file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,11 +11,10 @@ echo "<html>
     Holberton School
   </body>
 </html>" >/data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
 ln -snf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-my_string="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;/\n\t}\n
-FILE=/etc/nginx/sites-available/default"
+my_string="\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+FILE=/etc/nginx/sites-available/default
 result=$(grep "$my_string" "$FILE")
 if [ -z "$result" ]; then
     sed -i "26 a $my_string" "$FILE"
